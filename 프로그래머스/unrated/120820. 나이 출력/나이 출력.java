2a4ac559f4fa6c93cc0b50988b0654e63758a961
Eun Young <103404357+key1018@users.noreply.github.com>
2023-10-20 16:51:47 +0900

    [level unrated] Title: 나이 출력, Time: 11.81 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+class Solution {
+    public int solution(int age) {
+        LocalDate now = LocalDate.now();
+        int year = now.getYear();
+        return year - age;
+    }
+}
