cc3333d7df56930b183a5b3d267cb92062c6f2ef
Eun Young <103404357+key1018@users.noreply.github.com>
2023-10-20 15:50:16 +0900

    [level unrated] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+    
+        int answer = 1;
+        
+        while(true){
+            if((answer * 6) % n == 0){
+                break;
+            } else {
+                answer++;
+            }  
+        }
+            return answer;
+    }
+
+}
