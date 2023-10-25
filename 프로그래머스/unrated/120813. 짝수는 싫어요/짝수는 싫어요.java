a2dce296e2d173d935fc91ec9de442391a2a31b2
Eun Young <103404357+key1018@users.noreply.github.com>
2023-10-25 15:33:21 +0900

    [level unrated] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,22 +1,18 @@
 class Solution {
     public int[] solution(int n) {
-
-        int[] array = new int[100];
+        int[] answer = new int[100];
         int index = 0;
-
-        for(int i = 1; i <= n; i++){
+        for(int i = 0; i <= n; i++){
             if(i % 2 != 0){
-                array[index] = i;
+                answer[index] = i;
                 index++;
             }
         }
         
         int[] result = new int[index];
-        for(int i =0; i < index; i++){
-            result[i] = array[i];
+        for(int i = 0; i < index; i++){
+            result[i] = answer[i];
         }
-        
         return result;
-        
     }
 }
