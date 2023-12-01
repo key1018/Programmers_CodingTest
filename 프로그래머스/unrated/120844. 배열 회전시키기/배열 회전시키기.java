4e4736dff6a53dcf4d63633fa1d823ab98c8edfb
Eun Young <103404357+key1018@users.noreply.github.com>
2023-12-01 14:49:06 +0900

    [level unrated] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120844. 배열 회전시키기/배열 회전시키기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(int[] numbers, String direction) {
+        int[] answer = new int[numbers.length];
+        if(direction.equals("right")){
+            for(int i = 0; i < answer.length; i++){
+                if(i == 0){
+                    answer[0] = numbers[numbers.length - 1];
+                } else {
+                    answer[i] = numbers[i - 1];
+                }
+            }
+        } else {
+            for(int i = 0; i < answer.length; i++){
+                if(i == numbers.length - 1){
+                    answer[i] = numbers[0];
+                } else {
+                    answer[i] = numbers[i + 1];
+                }
+            }
+        }
+        return answer;
+    }
+}
