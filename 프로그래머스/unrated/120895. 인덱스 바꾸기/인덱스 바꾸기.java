23a87f644a6a8ee19f567c062ecc161f5f37bfb9
Eun Young <103404357+key1018@users.noreply.github.com>
2023-12-13 22:29:35 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -2,17 +2,24 @@ class Solution {
     public String solution(String my_string, int num1, int num2) {
         String answer = "";
         
-        for(int i = 0; i < my_string.length(); i++){
-            if(i == num1) {
-                answer += my_string.charAt(num2) + "";
-            } else if (i == num2) {
-                answer += my_string.charAt(num1) + "";
-            } else {
-                answer += my_string.charAt(i) + "";
-            }
-        }
+        // for(int i = 0; i < my_string.length(); i++){
+        //     if(i == num1) {
+        //         answer += my_string.charAt(num2) + "";
+        //     } else if (i == num2) {
+        //         answer += my_string.charAt(num1) + "";
+        //     } else {
+        //         answer += my_string.charAt(i) + "";
+        //     }
+        // }
         
+        char [] c = my_string.toCharArray();
+        
+        c[num1] = my_string.charAt(num2);
+        c[num2] = my_string.charAt(num1);
+        
+        answer = String.valueOf(c);
         
         return answer;
+
     }
 }
