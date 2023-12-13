bcaa8f76015c9dd97bf7484ce1af14d6702a6165
Eun Young <103404357+key1018@users.noreply.github.com>
2023-12-13 18:26:08 +0900

    [level unrated] Title: 중복된 문자 제거, Time: 1.21 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string) {
+        return Arrays.asList(my_string.split("")).stream()
+            .distinct()
+            .collect(Collectors.joining());
+    }
+}
